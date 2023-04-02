442c8cc397da0939ba3446b4a48bc1ceb9bb4410
Desykashalu12210753 <12210753@bsi.ac.id>
2023-04-02 08:14:31 -0700

    pendataanwarga

---
+++ apk2/android/app/src/main/kotlin/com/desyka31/apk2/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.desyka31.apk2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ apk2/lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:apk2/views/login_view.dart';
+
+main(List<String> args) {
+  runApp(const MaterialApp(
+    home: LoginView(),
+  ));
+}
